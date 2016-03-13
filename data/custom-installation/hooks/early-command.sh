b91b34e28c684084bdee5a79f2b1091b5c8f61d9
Hakuna Matata <hakuna1234567890@hotmail.com>
2016-03-13 23:39:32 +0100

    Fix for parted >= 3.2

--- data/custom-installation/hooks/early-command.sh
+++ data/custom-installation/hooks/early-command.sh
@@ -7,13 +7,6 @@ if [ -f /custom-installation/hooks/post-installer.sh ]; then
 	chmod +x /root/usr/lib/ubiquity/target-config/10post-installer
 fi
 
-architecture=$(uname -m | sed s/i6/i3/)
-if [ -f /custom-installation/patch/$architecture/parted_server ]; then
-	cp /custom-installation/patch/$architecture/parted_server  /root/usr/bin/parted_server
-        cp /custom-installation/patch/$architecture/libparted.so.0.0.1 /root/lib/$architecture-linux-gnu/libparted.so.0.0.1
-        ln -s libparted.so.0.0.1 /root/lib/$architecture-linux-gnu/libparted.so.0
-fi
-
 if [ -f /custom-installation/patch/autopartition-loop ]; then
 	cp /custom-installation/patch/autopartition-loop  /root/bin/autopartition-loop
 fi
