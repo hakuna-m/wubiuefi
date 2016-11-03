5e2127749bfcb58dd994e4986c68b02b57c5b854
Hakuna Matata <hakuna1234567890@hotmail.com>
2016-11-03 15:43:21 +0100

    add support for NVMe devices (#26)

--- data/custom-installation/hooks/casper-bottom.sh
+++ data/custom-installation/hooks/casper-bottom.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#fix wrong linux device names of MMCs in preseed.cfg
+#fix wrong linux device names of MMCs and NVMe devices in preseed.cfg
 if [ -e /preseed.cfg ]; then
 	sed -i "s:\(d-i partman-auto/disk string /dev/mmcblk[0-9]\)p:\1:g" /preseed.cfg
+	sed -i "s:\(d-i partman-auto/disk string /dev/nvme[0-9][0-9]*n[0-9][0-9]*\)p:\1:g" /preseed.cfg
 fi
 
 
