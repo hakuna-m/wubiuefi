7e2a548919aa351e65ccf5705c74f1479e661cfe
Hakuna Matata <hakuna1234567890@hotmail.com>
2019-05-06 23:19:28 +0200

    Bump to 19.04 (#143)

--- data/custom-installation/hooks/post-installer.sh
+++ data/custom-installation/hooks/post-installer.sh
@@ -8,12 +8,21 @@ if [ -f /custom-installation/patch/loop-remount ]; then
 fi
 
 if [ -f /custom-installation/patch/grub-install ] && [ -f /custom-installation/patch/grub-install-efi ] ; then
-        if [ -d /sys/firmware/efi ]; then
-		mkdir -p /target/usr/local/sbin
-		cp /custom-installation/patch/grub-install /target/usr/local/sbin/grub-install
+	mkdir -p /target/usr/local/sbin
+	cp /custom-installation/patch/grub-install /target/usr/local/sbin/grub-install
+	if [ -d /sys/firmware/efi ]; then
 		chmod +x /target/usr/local/sbin/grub-install
 		cp /custom-installation/patch/grub-install-efi /target/usr/local/sbin/grub-install-efi
 		chmod +x /target/usr/local/sbin/grub-install-efi
+	fi
+	if [ -f /custom-installation/patch/grub-install ] ; then
+		cp /custom-installation/patch/grub-mkimage-lupin /target/usr/local/sbin/grub-mkimage-lupin
+		chmod +x /target/usr/local/sbin/grub-mkimage-lupin
+	fi
+	if [ -f /custom-installation/patch/lupin-grub-install ] ; then
+		mkdir -p /target/etc/grub.d
+		cp /custom-installation/patch/lupin-grub-install /target/etc/grub.d/10_lupin_grub_install
+		chmod +x /target/etc/grub.d/10_lupin_grub_install
 	fi
 fi
 
